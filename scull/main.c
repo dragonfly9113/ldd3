8f09a5c35770f6c2640659b5bd184ebbc863a8c6
Weilin Luo <weilin.luo@thundersoft.com>
2020-06-30 16:13:57 -0700

    scull: add /proc based debug functions

--- scull/main.c
+++ scull/main.c
@@ -72,9 +72,142 @@ int scull_trim(struct scull_dev *dev)
 }
 
 #ifdef SCULL_DEBUG	/* use proc only if debugging */
+/*
+ * The proc filesystem: function to read an entry
+ */
+int scull_read_procmem(char *buf, char **start, off_t offset,
+		int count, int *eof, void *data)
+{
+	int i, j, len = 0;
+	int limit = count - 80;	/* Don't print more than this */
 
-/* skip proc functions for now */
+	for (i = 0; i < scull_nr_devs && len <= limit; i++) {
+		struct scull_dev *d = &scull_devices[i];
+		struct scull_qset *qs = d->data;
+		if (down_interruptible(&d->sem))
+			return -ERESTARTSYS;
+		len += sprintf(buf + len, "\nDevice %i: qset %i, q %i, sz %li\n", 
+				i, d->qset, d->quantum, d->size);
+		for (; qs && len <= limit; qs = qs->next) {	/* scan the list */
+			len += sprintf(buf + len, " item at %p, qset at %p\n",
+					qs, qs->data);
+			if (qs->data && !qs->next) /* dump only the last item */
+				for (j = 0; j < d->qset; j++) {
+					if (qs->data[j])
+						len += sprintf(buf + len,
+								"    % 4i: %8p\n",
+								j, qs->data[j]);
+				}
+		}
+		up(&scull_devices[i].sem);
+	}
+	*eof = 1;
+	return len;
+}
 
+/*
+ * For now, the seq_file implementation will exist in parallel, The
+ * older read_procmem function sould maybe go away, though.
+ */
+
+/*
+ * Here are our sequence iteration methods. Our "position" is
+ * simply the device number.
+ */
+static void *scull_seq_start(struct seq_file *s, loff_t *pos)
+{
+	if (*pos >= scull_nr_devs)
+		return NULL;	/* No more to read */
+	return scull_devices + *pos;
+}
+
+static void *scull_seq_next(struct seq_file *s, void *v, loff_t *pos)
+{
+	(*pos)++;
+	if (*pos >= scull_nr_devs)	
+		return NULL;
+	return scull_devices + *pos;
+}
+
+static void scull_seq_stop(struct seq_file *s, void *v)
+{
+	/* Actually, there is nothing to do here */
+}
+
+static int scull_seq_show(struct seq_file *s, void *v)
+{
+	struct scull_dev *dev = (struct scull_dev *) v;
+	struct scull_qset *d;
+	int i;
+
+	if (down_interruptible(&dev->sem))
+		return -ERESTARTSYS;
+	seq_printf(s, "\nDevice %i: qset %i, q %i, sz %li\n",
+			(int) (dev - scull_devices), dev->qset,
+			dev->quantum, dev->size);
+	for (d = dev->data; d; d = d->next) {	/* scan the list */
+		seq_printf(s, " item at %p, qset at %p\n", d, d->data);
+		if (d->data && !d->next)	/* dump only the last item */
+			for (i = 0; i < dev->qset; i++) {
+				if (d->data[i])
+					seq_printf(s, "    % 4i: %8p\n",
+							i, d->data[i]);
+			}
+	}
+	up(&dev->sem);
+	return 0;
+}
+
+/*
+ * Tie the sequence operations up.
+ */
+static struct seq_operations scull_seq_ops = {
+	.start = scull_seq_start,
+	.next  = scull_seq_next,
+	.stop  = scull_seq_stop,
+	.show  = scull_seq_show
+};
+
+/*
+ * Now to implement the /proc file we need only make an open
+ * method which sets up the sequence operators.
+ */
+static int scull_proc_open(struct inode *inode, struct file *file)
+{
+	return seq_open(file, &scull_seq_ops);
+}
+
+/*
+ * Create a set of file operations for our proc file.
+ */
+static struct file_operations scull_proc_ops = {
+	.owner	 = THIS_MODULE,
+	.open	 = scull_proc_open,
+	.read	 = seq_read,
+	.llseek	 = seq_lseek,
+	.release = seq_release
+};
+
+/*
+ * Actually create (and remove) the /proc file(s).
+ */
+static void scull_create_proc(void)
+{
+	struct proc_dir_entry *entry;
+	create_proc_read_entry("scullmem", 0 /* default mode */,
+			NULL /* parent dir */, scull_read_procmem,
+			NULL /* client data */);
+	entry = create_proc_entry("scullseq", 0, NULL);
+	if (entry)
+		entry->proc_fops = &scull_proc_ops;
+}
+
+static void scull_remove_proc(void)
+{
+	/* no problem if it was not registered */
+	remove_proc_entry("scullmem", NULL /* parent dir */);
+	remove_proc_entry("scullseq", NULL);
+}
 
 #endif	/* SCULL_DEBUG */
 
