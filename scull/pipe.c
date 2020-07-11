33d78cf591e2738d64973e6aa6bb2b4b40f0b2d9
Weilin Luo <weilin.luo@thundersoft.com>
2020-07-10 18:49:30 -0700

    scull: add more to pipe.c

--- scull/pipe.c
+++ scull/pipe.c
@@ -241,18 +241,76 @@ struct file_operations scull_pipe_fops = {
 	.fasync = 	scull_p_fasync,
 };
 
+/*
+ * Set up a cdev entry
+ */
+static void scull_p_setup_cdev(struct scull_pipe *dev, int index)
+{
+	int err, devno = scull_p_devno + index;
 
+	cdev_init(&dev->cdev, &scull_pipe_fops);
+	dev->cdev.owner = THIS_MODULE;
+	err = cdev_add (&dev->cdev, devno, 1);
+	/* Fail gracefully if need be */
+	if (err)
+		printk(KERN_NOTICE "Error %d adding scullpipe%d\n", err, index);
+}
 
+/*
+ * Initialize the pipe devs; return how many we did.
+ */
+int scull_p_init(dev_t firstdev)
+{
+	int i, result;
 
+	result = register_chrdev_region(firstdev, scull_p_nr_devs, "scullp");
+	if (result < 0) {
+		printk(KERN_NOTICE "Unable to get scullp region, error %d\n", result);
+		return 0;
+	}
+	scull_p_devno = firstdev;
+	scull_p_devices = kmalloc(scull_p_nr_devs * sizeof(struct scull_pipe), GFP_KERNEL);
+	if (scull_p_devices == NULL) {
+		unregister_chrdev_region(firstdev, scull_p_nr_devs);
+		return 0;
+	}
+	memset(scull_p_devices, 0, scull_p_nr_devs * sizeof(struct scull_pipe));
+	for (i = 0; i < scull_p_nr_devs; i++) {
+		init_waitqueue_head(&(scull_p_devices[i].inq));		
+		init_waitqueue_head(&(scull_p_devices[i].outq));		
+		sema_init(&scull_p_devices[i].sem, 1);
+		scull_p_setup_cdev(scull_p_devices + i, i);
+	}
+#ifdef SCULL_DEBUG
+	/* obsolete */
+	//create_proc_read_entry("scullpipe", 0, NULL, scull_read_p_mem, NULL);
+#endif
+	return scull_p_nr_devs;
+}
 
+/*
+ * This is called by cleanup_module or on failure.
+ * It is required to never fail, even if nothing was initialized first
+ */
+void scull_p_cleanup(void)
+{
+	int i;
 
+#ifdef SCULL_DEBUG
+	/* obsolete */
+	//remove_proc_entry("scullpipe", NULL);
+#endif
 
+	if (!scull_p_devices)
+		return;	/* nothing else to release */
 
-
-
-
-
-
-
+	for (i = 0; i < scull_p_nr_devs; i++) {
+		cdev_del(&scull_p_devices[i].cdev);
+		kfree(scull_p_devices[i].buffer);
+	}
+	kfree(scull_p_devices);	
+	unregister_chrdev_region(scull_p_devno, scull_p_nr_devs);
+	scull_p_devices = NULL;	/* pedantic */
+}
 
 
